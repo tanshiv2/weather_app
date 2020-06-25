966a41290164696bf69e00aa0801bbe877f386d1
tanshiv2 <shivangi.tanwar4@gmail.com>
2020-06-25 11:50:58 +0530

    Client side nodejs scripts

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+console.log('This is the js page')
+
+const messageOne = document.querySelector('#first')
+const messageTwo = document.querySelector('#second')
+const messageThree = document.querySelector('#third')
+const icon = document.querySelector('#icon')
+
+const fetchData = (location) => {fetch('http://localhost:3000/weather?location=' + location).then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            messageOne.textContent = ''
+            messageThree.textContent = 'Error'
+            messageTwo.textContent = data.error
+        } else {
+            const icon = document.createElement('img')
+            icon.src = data.img
+            messageOne.textContent = data.location
+            messageThree.textContent = data.description + '  '
+            messageThree.appendChild(icon)
+            messageTwo.textContent = data.forecast
+        }
+    })
+})
+}
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    
+    e.preventDefault()
+
+    messageOne.textContent = 'Loading'
+
+    const location = search.value
+
+    fetchData(location)
+
+    console.log(location)
+})
